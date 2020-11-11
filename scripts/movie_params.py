dadc9bf0e939b53fa2ecf87e6a0fe7196ee505f4
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2020-11-11 11:24:24 +0100

    / Enable 1min GOES16 images downloaded with observingCloud/satdownload
    + possibility to load 1min resolution sat data
    + add option to change GOES16 channel name in movie_params
    + update dropsonde reader to read JOANNE L3 data
    + parameter to define data input fileformat
    + parameter to define machine (ciclad/local)
    / make more use of the definitions in movie_params (WIP)
    + progress bar in make_figure_from_data.py

--- scripts/movie_params.py
+++ scripts/movie_params.py
@@ -13,7 +13,13 @@ elif getpass.getuser() == "ludo":
   goesdir ='../images/ciclad'
   platformdir = "/media/ludo/DATA/google-drive/Thèse/EUREC4a/github/Input/"
   outputdir ='../movies/'
+  outputdir_images = '../images/ciclad'
 else:
+  sondedir = '../data/dropsondes'
+  platformdir = '../data/tracks'
+  goesdir = '/scratch/local1/m300408/GOES16animation/images'
+  image_file_fmt = 'GOES16__{channel}__%Y%m%d_%H%M.png'
+  outputdir = '../movies'
   pass # ADD YOUR DIRECTORIES HERE
 
 
@@ -25,6 +31,10 @@ lon_center, lat_center = -57.717,13.3
 lon_pt_circle, lat_pt_circle = -57.245,14.1903
 r_circle = np.sqrt((lon_pt_circle-lon_center)**2+(lat_pt_circle-lat_center)**2)
 
+# GOES16 data
+GOES16_var_night = 'C13'  # channel variable to use during nighttime (e.g. C13, temp_11_0um_nom)
+GOES16_var_day = 'C13'   # channel variable to use during daytime (e.g. C02, refl_0_65um_nom)
+
 # Image box
 lonmin,lonmax = -60,-55
 dlon = lonmin-lonmax
@@ -34,8 +44,8 @@ width = 1125
 height = int(width*dlat/dlon)
 
 # time range
-start_time = "12:00"
-end_time = "15:00"
+start_time = "10:30"
+end_time = "21:30"
 
 # start_time = "00:00"
 # end_time = "23:59"
